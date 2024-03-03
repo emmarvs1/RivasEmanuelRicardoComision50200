2be454ea98306fb9dc5d13e6ee8d5b9cb88b9cb6
Emanuel Rivas <emanuelrrivas@hotmail.com>
2024-03-03 17:27:16 -0300

    mas modif

--- proyecto/aplicacion/forms.py
+++ proyecto/aplicacion/forms.py
@@ -36,7 +36,7 @@ class RegistroForm(UserCreationForm):
     password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Confirmar Contraseña", widget=forms.PasswordInput)
 
-    class Meta:
+class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
     
@@ -47,7 +47,7 @@ class UserEditForm(UserCreationForm):
     first_name = forms.CharField(label="Nombre/s", max_length=50, required=True)
     last_name = forms.CharField(label="Apellido/s", max_length=50, required=True)
 
-    class Meta:
+class Meta:
         model = User
         fields = ['email', 'password1', 'password2', 'first_name', 'last_name']
         
